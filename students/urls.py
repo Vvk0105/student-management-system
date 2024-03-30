c677d75149020b5270a94dda7a0311f07e05d3a4
Vivek Babu <vivekbabu0105@gmail.com>
2024-03-30 23:15:11 +0530

    add student feature implimented without any design

--- students/urls.py
+++ students/urls.py
@@ -20,4 +20,6 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('<int:id>',views.view_student,name='view_student'),
+    path('add',views.add,name='add'),
+    #path('views/', views.views_page, name='views_page'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Student
+from .forms import StudentForm
 # Create your views here.
 def index(request):
     return render(request,'index.html',{'students':Student.objects.all()})
@@ -10,3 +11,19 @@ def view_student(request, id):
     student=Student.objects.get(pk=id)
     return HttpResponseRedirect(reverse('index'))
     
+def add(request):
+    form=StudentForm()
+    if request.POST:
+        form=StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            form=StudentForm()
+    return render(request,'add.html',{'form':form})
+
+            
+
+#def views_page(request):
+#    student_id = request.GET.get('id')  # Retrieve student ID from URL parameter
+#    student = get_object_or_404(Student, pk=student_id)  # Fetch student details
+#    return render(request, 'views.html', {'student': student})
